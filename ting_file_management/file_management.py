c305e79e6743d7a6d5fa40ed85caf9558b4fc1df
Vanessa <vanessamrios@gmail.com>
2022-09-16 18:05:33 +0200

    feat: implementa a função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str) -> list:
+    if (path_file.endswith(".txt") is False):
+        sys.stderr.write("Formato inválido\n")
+
+    try:
+        with open(path_file, "r") as file:
+            return file.read().split("\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+# https://codare.aurelio.net/2007/01/30/python-imprimir-mensagens-de-erro-stderr/
+# https://stackoverflow.com/questions/28633555/how-to-handle-filenotfounderror-when-try-except-ioerror-does-not-catch-it
