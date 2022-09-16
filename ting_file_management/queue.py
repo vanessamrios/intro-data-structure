dda8f8b3571296ec707e243eb884dc5b94642672
Vanessa <vanessamrios@gmail.com>
2022-09-16 15:25:15 +0200

    feat: implementa a fila para armazenar os arquivos que serão lidos

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,21 @@
 class Queue:
     def __init__(self):
-        """Inicia o projeto"""
+        self.data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.data.append(value)
+
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if (len(self.data) == 0):
+            raise IndexError
+        return self.data.pop(0) 
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if (index < 0):
+            raise IndexError
+        else:
+            return self.data[index]
