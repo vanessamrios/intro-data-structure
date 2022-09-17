682641aed14cf1dbe45a81e8b6f3626f67e892e6
Vanessa <vanessamrios@gmail.com>
2022-09-17 14:28:00 +0200

    feat: implementa a função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,29 @@
+import sys
+
+from ting_file_management.file_management import txt_importer
+
+
+def file_exists(path_file, instance) -> bool:
+    for index in range(0, len(instance)):
+        if (instance.search(index) == path_file):
+            return True
+    return False
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if file_exists(path_file, instance):
+        return
+
+    file = txt_importer(path_file)
+    instance.enqueue(path_file)
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+
+    sys.stdout.write(str(result))
 
 
 def remove(instance):
